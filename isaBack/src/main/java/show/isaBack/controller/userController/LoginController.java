c55feeb197d0bc83fca50c98ec3328dea1c4bca0
Nemanja Dragutinovic <nemanjadragutinovic54@gmail.com>
2021-04-17 16:24:31 +0200

    fixing login auth

--- isaBack/src/main/java/show/isaBack/controller/userController/LoginController.java
+++ isaBack/src/main/java/show/isaBack/controller/userController/LoginController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,41 +25,47 @@ import show.isaBack.DTO.userDTO.UserTokenDTO;
 import show.isaBack.model.User;
 import show.isaBack.security.auth.AuthRequest;
 import show.isaBack.secutiry.TokenUtils;
+import show.isaBack.service.userService.UserService;
 
 @RestController
 @RequestMapping(value = "/log")
 public class LoginController {
-	
+
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
 	@Autowired
 	private TokenUtils tokenUtils;
 	
+	@Autowired
+	private UserService userService;
 	
-	
-	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@PostMapping("/login")
 	@CrossOrigin
 	public ResponseEntity<UserTokenDTO> createAuthenticationToken(@RequestBody AuthRequest authRequest,
 			HttpServletResponse response) {
-		
+
 		String jwt;
 		int expiresIn;
 		List<String> roles = new ArrayList<String>();
 		
-		
+		System.out.println(passwordEncoder.encode(authRequest.getPassword()));
+
 		try {
-			Authentication authentication = authenticationManager
-					.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(),
-							authRequest.getPassword()));
-			
+			Authentication authentication = authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+
 			System.out.println("prosao autentifikaciju");
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 			User user = (User) authentication.getPrincipal();
 			jwt = tokenUtils.generateToken(user.getUsername());
 			expiresIn = tokenUtils.getExpiredIn();
 			user.getUserAuthorities().forEach((a) -> roles.add(a.getName()));
+			
+			System.out.println(authRequest.getUsername() + authRequest.getPassword() + "AUTENTIFIKACIJA");
 
 		} catch (BadCredentialsException e) {
 			System.out.println("bad credentials");
@@ -69,8 +76,10 @@ public class LoginController {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
-		System.out.println("prosao do kraja");
 		
+
+		System.out.println("prosao do kraja");
+
 		return new ResponseEntity<UserTokenDTO>(new UserTokenDTO(jwt, expiresIn, roles), HttpStatus.OK);
 	}
 
