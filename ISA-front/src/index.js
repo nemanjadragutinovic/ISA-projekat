466e5e2ffae2066625ed43ca6334eacb940b3154
Stefan Zec <stefanzec@hotmail.rs>
2021-07-24 16:32:09 +0200

    specifikacijaProjekta

--- ISA-front/src/index.js
+++ ISA-front/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 ReactDOM.render(
   <React.StrictMode>
